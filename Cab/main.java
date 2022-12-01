68cc735019839b7478d618f7952c82044de3f8ff
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-12-01 22:55:15 +0530

    Update main.java

--- Cab/main.java
+++ Cab/main.java
@@ -1,33 +1,39 @@
-import java.util.*
+import java.util.*;
 import java.util.Scanner;
 
-import cab.exceptions;
-import cab.actors;
+import Exceptions.DriverUnavailable;
+import Actors.User;
 
 // Import the packages created 
 
-public class main{
+public class Main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        CabService NewCab = new CabService.getCab();
-        Source start = new Source();
-        Destination end = new Destination();
-        Student student = null;
+
+        // CabService NewCab = new CabService.getCab();
+        // Source start = new Source();
+        // Destination end = new Destination();
+        // Student student = null;
         
         // Implementing Switch case for different operations;
-        
+    
         System.out.println("Enter an option");
         char c = in.next().charAt(0);
-        
-        Switch(c){
+        switch(c){
+            case 'S': System.out.println("Enter your name, ID and phone no.\n");
+            String name = in.next();
+            int id=in.nextInt();
+            int PhoneNumber=in.nextInt();
+            User NewStudent = new User(name,id,PhoneNumber);
+            NewStudent.ShowDetails();
+            break;
+        }
+        in.close();
+        // try{
+        //     student=CabService.RegisterStudent();
+        // }
+        // catch{
             
-        }
-        
-        try{
-            student=CabService.RegisterStudent();
-        }
-        catch{
-            
-        }
+        // }
     }
 }
