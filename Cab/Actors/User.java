259d92d9f3063d15bdc3d2eeca00771593056a7c
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-12-01 17:10:07 +0530

    Create User.java

---
+++ Cab/Actors/User.java
@@ -0,0 +1,38 @@
+package cab.actors
+
+public abstract class User{
+    private String name;
+    private int id;
+    private int PhoneNumber;
+    
+    public User(String name, int id, int PhoneNumber){
+        this.name=name;
+        this.id=id;
+        this.PhoneNumber=PhoneNumber;
+    }
+    
+    public ShowDetails(){
+        System.out.println("Registration Successful\n")
+        System.out.println("Entered Details : \n");
+        System.out.println("Name : \n"+name);
+        System.out.println("ID : \n"+id);
+        System.out.println("Phone no. : \n"+PhoneNumber);
+        
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
