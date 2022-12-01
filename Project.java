cff42e1f8cf96145ca42ee90c45ac68af64e70dd
Rudra Jewalikar <98890074+RudraJewalikar@users.noreply.github.com>
2022-12-01 16:35:55 +0530

    Create Project.java

---
+++ Project.java
@@ -0,0 +1,95 @@
+public class Project {
+	public static void main(String[] args)
+	{
+		switch case
+	}
+	
+		S()
+		{
+		/*
+		 * iterate over list of students to see if student exists
+		 * create new student if required and add it to list
+		 */
+      Student 
+		}
+		T()
+		{
+			/*
+			 *  will create an object of type recquirement 
+			 *  Map it with list of Trips
+			 *  create one more map of ID,trip			
+			*/
+		}
+		C()
+		{
+			/*
+			 * iterate over map of ID,trip
+			 * might have to create new equals method 
+			 */
+		}
+		A()
+		{
+			/*
+			 * add or remove from the map
+			 */
+		}
+		}
+		R()
+		{
+			
+		}
+		L1()
+		{
+			
+		}
+		AC()
+		{
+			
+		}
+		AS()
+		{
+			
+		}
+		E()
+		{
+			
+		}
+		L2()
+		{
+			
+		}
+		
+}
+class Student
+{
+	username
+	id
+	phone_number
+	charge
+	object of type requirements
+	//Create a constructor
+	
+}
+class Admin{
+	username
+	password
+}
+abstract class List
+{
+ static List of students// map of <ID, student>
+ static List of Admin // map of <username , password>
+}
+class Requirements
+{
+	date
+	source
+	destination
+	time of departure 
+}
+class Trip
+{
+	trip ID
+	Cab ID
+	list<travellers>
+	
+}
