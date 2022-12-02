0afa1ed01b4e5d108b1f4ec3935f42f1c487d2eb
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-12-02 19:30:06 +0530

    Update User.java

--- Cab/Actors/User.java
+++ Cab/Actors/User.java
@@ -1,29 +1,58 @@
 package Actors;
 
-public class User{
+import java.util.*;
+
+import Exceptions.*;
+
+public class User {
     private String name;
     private int id;
-    private int PhoneNumber;
-    
-    public User(String name, int id, int PhoneNumber){
-        this.name=name;
-        this.id=id;
-        this.PhoneNumber=PhoneNumber;
+    private long PhoneNumber;
+
+    public User(String name, int id, long PhoneNumber) {
+        this.name = name;
+        this.id = id;
+        this.PhoneNumber = PhoneNumber;
     }
-    
-    public void ShowDetails(){
+
+    private User(){
+
+    }
+
+    private static User username = null;
+    public static User getInstance() {
+        if (username == null) {
+            username = new User();
+        }
+        return username;
+    }
+
+    public Map<Integer, User> userMap = new HashMap<Integer, User>();
+    public Map<Integer, User> driverMap = new HashMap<Integer, User>();
+
+    public void ShowDetails() {
         System.out.println(" ");
         System.out.println("---------------------------------------------------------------------------------------");
         System.out.println("Registration Successful");
         System.out.println(" ");
         System.out.println("Entered Details - ");
-        System.out.println("Name     \t : \t"+this.name);
-        System.out.println("ID       \t : \t"+this.id);
-        System.out.println("Phone no.\t : \t"+this.PhoneNumber);
+        System.out.println("Name     \t : \t" + this.name);
+        System.out.println("ID       \t : \t" + this.id);
+        System.out.println("Phone no.\t : \t" + this.PhoneNumber);
         System.out.println(" ");
         System.out.println("---------------------------------------------------------------------------------------");
     }
-    
+
+    public User registerUser(String name, int id, long PhoneNumber)throws CreateException{
+        User user = new User(name, id, PhoneNumber);
+        if(user==null||userMap.containsKey(user.getId())){
+            throw new CreateException("User already registered !!");
+        }
+        userMap.put(user.getId(), user);
+        return user;
+    }
+
+
     public String getName() {
         return name;
     }
@@ -31,12 +60,12 @@ public class User{
     public void setName(String name) {
         this.name = name;
     }
-    
-    public int getPhone() {
+
+    public long getPhone() {
         return PhoneNumber;
     }
 
-    public void setPhone(int PhoneNumber) {
+    public void setPhone(long PhoneNumber) {
         this.PhoneNumber = PhoneNumber;
     }
 
