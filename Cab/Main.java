a781f9a630c8dfe78240aee70d6da71c1a535ada
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-12-02 19:29:28 +0530

    Update Main.java

--- Cab/Main.java
+++ Cab/Main.java
@@ -1,125 +1,79 @@
+package Actors;
+
 import java.util.*;
-import java.util.Scanner;
-import java.text.SimpleDateFormat;
 
-import Exceptions.DriverUnavailable;
-import Actors.*;
+import Exceptions.*;
 
-// Import the packages created 
+public class User {
+    private String name;
+    private int id;
+    private long PhoneNumber;
 
-public class Main {
-    public static void main(String[] args) {
-
-        // CabService NewCab = new CabService.getCab();
-        // Source start = new Source();
-        // Destination end = new Destination();
-        // Student student = null;
-
-        // Implementing Switch case for different operations;
-        System.out.println(" ");
-        System.out.println("Welcome to the Cab Booking System created by Group 3");
-        System.out.println("-----------------------------------------------------");
-        System.out.println("Available options\n");
-        System.out.println("S  - Student registeration and Details");
-        System.out.println("T  - Requesting a new Trip");
-        System.out.println("C  - Check details of a proposed Trip");
-        System.out.println("A  - Accept a Trip");
-        System.out.println("R  - Reject a Trip");
-        System.out.println("L1 - Admin Login");
-        System.out.println("X  - Exit");
-        System.out.println("-----------------------------------------------------\n");
-        boolean cont = true;
-        while (cont) {
-            
-            System.out.print("Enter an option ");
-            Scanner in = new Scanner(System.in);
-            char c = in.next().charAt(0);
-            int id;
-            String name;
-            if (c == 'X') {
-                System.out.println("Good Bye !!");
-                cont = false;
-            } else {
-                switch (c) {
-
-                    // New User is being registered
-                    // Add him in a list
-                    case 'S':
-                        System.out.println("Enter your name, ID and phone no.");
-                        name = in.next();
-                        id = in.nextInt();
-                        long PhoneNumber = in.nextLong();
-                        User NewStudent = new User(name, id, PhoneNumber);
-                        NewStudent.ShowDetails();
-                        System.out.println("Continue (y/n)...");
-                        char test = in.next().charAt(0);
-                        if(test!='y'){
-                            cont=false;
-                        }
-                        break;
-
-                    // New trip is getting created
-                    // Add it into a List
-                    case 'T':
-                        System.out.println("Make a new trip (y/n)....");
-                        char trip = in.next().charAt(0);
-                        if (trip == 'y') {
-                            System.out.println("Enter your ID");
-                            id = in.nextInt();
-                            System.out.println("Enter your date of travel in (dd-mm-yyyy) format");
-                            String date = in.next();
-                            System.out.println("Enter your source location");
-                            String source = in.next();
-                            System.out.println("Enter your destination location");
-                            String destination = in.next();
-                            System.out.println("Enter your departure time in (hh:mm) format");
-                            String time_string = in.next();
-                            Trip NewTrip = new Trip(id, date, source, destination, time_string);
-                            NewTrip.ShowDetails();
-                        } else {
-                            System.out.println("Good Bye !!");
-                            cont = false;
-                        }
-                        break;
-
-                    case 'C':
-                        System.out.println("Enter the ID to view the details of your Trip");
-
-                        // If the ID has an active trip, show the details using showDetails()
-                        // How to call that method using the right parameters ??
-                        // How to get the parameters using the ID (Getters and Setters)
-                        // Appropriate message displayed if ID has no linked travels
-
-                        id = in.nextInt();
-                        break;
-
-                    case 'A':
-                        System.out.println("Enter your ID to accept a trip");
-
-                        // flag value set to 1 : Accepted trip
-                        // showDetails() function call
-                        // if user has an active trip, give him option to accept
-                        // otherwise proper message : Please create a trip first !!
-                        id = in.nextInt();
-                        break;
-
-                    case 'R':
-                        System.out.println("Enter your ID to reject a trip");
-                        // flag value set to 0 : Rejected trip
-                        // showDetails() function call
-                        // if user has an active trip, give him option to reject
-                        // otherwise proper message : Please create a trip first !!
-                        id = in.nextInt();
-                        break;
-                }
-                // try{
-                // student=CabService.RegisterStudent();
-                // }
-                // catch{
-
-                // }
-            }
-        }
+    public User(String name, int id, long PhoneNumber) {
+        this.name = name;
+        this.id = id;
+        this.PhoneNumber = PhoneNumber;
     }
 
+    private User(){
+
+    }
+
+    private static User username = null;
+    public static User getInstance() {
+        if (username == null) {
+            username = new User();
+        }
+        return username;
+    }
+
+    public Map<Integer, User> userMap = new HashMap<Integer, User>();
+    public Map<Integer, User> driverMap = new HashMap<Integer, User>();
+
+    public void ShowDetails() {
+        System.out.println(" ");
+        System.out.println("---------------------------------------------------------------------------------------");
+        System.out.println("Registration Successful");
+        System.out.println(" ");
+        System.out.println("Entered Details - ");
+        System.out.println("Name     \t : \t" + this.name);
+        System.out.println("ID       \t : \t" + this.id);
+        System.out.println("Phone no.\t : \t" + this.PhoneNumber);
+        System.out.println(" ");
+        System.out.println("---------------------------------------------------------------------------------------");
+    }
+
+    public User registerUser(String name, int id, long PhoneNumber)throws CreateException{
+        User user = new User(name, id, PhoneNumber);
+        if(user==null||userMap.containsKey(user.getId())){
+            throw new CreateException("User already registered !!");
+        }
+        userMap.put(user.getId(), user);
+        return user;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhone() {
+        return PhoneNumber;
+    }
+
+    public void setPhone(long PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
