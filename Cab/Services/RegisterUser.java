8ef03b3c68a32f025def16a6cd0a8dcf9534bcc2
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-12-03 11:02:11 +0530

    Create RegisterUser.java

---
+++ Cab/Services/RegisterUser.java
@@ -0,0 +1,37 @@
+package Cab;
+
+import Actors.*;
+import Exceptions.*;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class RegisterTrip extends Trip {
+    private String id;
+    private String date;
+    private String source;
+    private String destination;
+    private String time;
+
+    public RegisterTrip(String id, String date, String source, String destination, String time) {
+        super(id, date, source, destination, time);
+    }
+
+    public static Map<String, Trip> userTrip = new HashMap<String, Trip>();
+
+    public void register(Trip trip) throws CreateException {
+        // if (trip == null || userTrip.containsKey(trip.getId())) {
+        // System.out.println("User already registered with the same ID!!");
+        // throw new CreateException("User already registered !!");
+        // }
+        // Put the ID and trip object in the userMap
+        userTrip.put(trip.getId(), trip);
+
+        // To print the trip details
+        trip.ShowDetails();
+
+        for (Map.Entry<String, Trip> entry : userTrip.entrySet())
+            System.out.println("Key = " + entry.getKey() +
+                    ", Value = " + entry.getValue());
+    }
+}
