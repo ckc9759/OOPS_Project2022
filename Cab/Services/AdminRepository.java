3f2e23dd725e50d30a2d3209269856ac67754ece
Rudra Jewalikar <98890074+RudraJewalikar@users.noreply.github.com>
2022-12-04 11:54:19 +0530

    Create AdminRepository.java

---
+++ Cab/Services/AdminRepository.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import Actors.User;
+import Exceptions.CreateException;
+
+ class AdminRepsitory {
+	public static Map<String, Admin> AdminMap = new HashMap<String,Admin>();
+	// manually fill AdminMap
+}
