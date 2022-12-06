6963e5b650da37f69da5d7c3dc808aac0a5f0342
Chaitanya Krishna Chauhan <95117634+ckc1404@users.noreply.github.com>
2022-12-06 19:34:15 +0530

    Add files via upload

---
+++ Final_Submission/Fares/Fares.java
@@ -0,0 +1,18 @@
+package Fare;
+
+import java.util.*;
+
+public class Fares {
+    public static String getCost(String str) {
+        if (str.equals("PilaniDelhi") || str.equals("DelhiPilani")) {
+            return Integer.toString(3500);
+        } else if (str.equals("PilaniJaipur") || str.equals("JaipurPilani")) {
+            return Integer.toString(3000);
+        } else if (str.equals("JaipurDelhi") || str.equals("DelhiJaipur")) {
+            return Integer.toString(3500);
+        }
+        return Integer.toString(5000);
+    }
+
+    public static TreeMap<String, String> farelist;
+}
